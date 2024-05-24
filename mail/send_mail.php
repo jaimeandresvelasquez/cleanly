56f4625d66711babf9de74ed8c82bf9f01b84031
jaimeandresvelasquez <jaimeandresvelasqueza@gmail.com>
2024-05-24 18:29:13 -0500

    contacto con correo

---
+++ mail/send_mail.php
@@ -0,0 +1,24 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "jaimeandresvelasqueza@gmail.com"; // Cambia esto a tu correo electrónico
+$subject = "$m_subject:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email\r\n";
+$header .= "Reply-To: $email\r\n";
+
+if(!mail($to, $subject, $body, $header)) {
+  http_response_code(500);
+  exit();
+}
+http_response_code(200);
+?>
+
